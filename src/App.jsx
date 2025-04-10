702fe987a9ecb9851163aa75ebac8303c026664b
manansaipi <abdulmannan.saipi@gmail.com>
2025-04-10 08:14:41 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "./components/Home/home";
+import About from "./components/About/about";
+
+const App = () => {
+  return (
+    <>
+      <Home />
+      <About />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/About/about.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section bg-red-500">
+      <div className="About">
+        <h1>Hi, I'm Abdul Mannan Saipi</h1>
+      </div>
+    </section>
+  );
+};
+
+export default About;
