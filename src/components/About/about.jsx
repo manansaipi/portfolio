3ba782b92ea8af2d24218fe37be52e29862c2473
manansaipi <abdulmannan.saipi@gmail.com>
2025-04-25 16:18:25 +0700

    about section

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 import ComingSoon from "../ComingSoon/ComingSoon";
 
+gsap.registerPlugin(ScrollTrigger);
+
 const About = () => {
+    const aboutRef = useRef();
+
+    // let tl = gsap.timeline({
+    //     // yes, we can add it to an entire timeline!
+    //     scrollTrigger: {
+    //         trigger: aboutRef.current,
+    //         markers: true,
+    //         pin: true, // pin the trigger element while active
+    //         start: "top center",
+    //         // end: "top top", // end after scrolling 500px beyond the start
+    //     },
+    // });
     return (
         <section id="About" className="">
             <ComingSoon></ComingSoon>
-            {/* <div className="bg-black w-full h-[30vh]">hey</div> */}
+            {/* <div ref={aboutRef} className="bg-black w-full h-[30vh]">
+                hey
+            </div> */}
         </section>
     );
 };
