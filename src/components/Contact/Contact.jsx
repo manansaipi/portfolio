c895e9f454fa26f011a7fba1811f2d17f28366ff
manansaipi <abdulmannan.saipi@gmail.com>
2025-05-02 17:29:11 +0700

    add lanyard

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import bgImage from "../../assets/img/bg/noise-transparent.png"; // adjust the path as needed
 import FooterContact from "./FooterContact";
+import Lanyard from "./Lanyard";
 
 const Contact = () => {
   return (
@@ -8,7 +9,8 @@ const Contact = () => {
       className="section bg-gray-50 bg-repeat bg-center"
       style={{ backgroundImage: `url(${bgImage})` }}
     >
-      <div className="h-[80vh] "></div>
+      <Lanyard position={[0, 0, 20]} gravity={[0, -40, 0]} />
+      {/* <div className="h-[80vh] "></div> */}
       <FooterContact />
     </div>
   );
