fb72360b944be166258ef7f623de655f764ca3a0
manansaipi <abdulmannan.saipi@gmail.com>
2025-05-05 09:46:13 +0700

    fix contecnt overflwo in certificates section

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -3,11 +3,11 @@ import ComingSoon from "../ComingSoon/ComingSoon";
 import Certificate from "./Certificate/Certificate";
 
 const Experience = () => {
-	return (
-		<section id="Experience" className="h-screen bg-light-dark">
-			<Certificate />
-		</section>
-	);
+  return (
+    <section id="Experience" className="pb-20 md:h-screen bg-light-dark">
+      <Certificate />
+    </section>
+  );
 };
 
 export default Experience;
