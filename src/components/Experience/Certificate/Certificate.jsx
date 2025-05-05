1fc9e65fe8ebcec96993a18de19097a7c840413e
manansaipi <abdulmannan.saipi@gmail.com>
2025-05-05 08:17:28 +0700

    show and hide certificate logic

--- src/components/Experience/Certificate/Certificate.jsx
+++ src/components/Experience/Certificate/Certificate.jsx
@@ -7,110 +7,116 @@ import certificates from "./CertificatesList";
 gsap.registerPlugin(ScrollTrigger);
 
 const Certificate = () => {
-	const [hoveredIndex, setHoveredIndex] = useState(null);
-	const [position, setPosition] = useState({ x: 0, y: 0 });
-	const [windowWidth, setWindowWidth] = useState(window.innerWidth); // Track window width
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+  const [showCertificate, setShowCertificate] = useState(false); // new line
 
-	const certDescContainerRef = useRef();
-	const certcertListRef = useRef();
-	const certDescRef = useRef();
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth); // Track window width
 
-	const certListRef = useRef();
+  const certDescContainerRef = useRef();
+  const certcertListRef = useRef();
+  const certDescRef = useRef();
 
-	const hoveredIndexRef = useRef(null);
+  const certListRef = useRef();
 
-	useEffect(() => {
-		hoveredIndexRef.current = hoveredIndex;
-	}, [hoveredIndex]);
+  const hoveredIndexRef = useRef(null);
 
-	useEffect(() => {
-		const moveCursor = (e) => {
-			// set delay to have magnetic effect
-			setTimeout(() => {
-				let currentIndex = hoveredIndexRef.current;
-				// console.log(currentIndex)
-				if (currentIndex !== null) {
-					certcertListRef.current.textContent = certificates[currentIndex].name;
-					certDescRef.current.textContent = certificates[currentIndex].desc;
-				}
+  useEffect(() => {
+    hoveredIndexRef.current = hoveredIndex;
+  }, [hoveredIndex]);
 
-				setPosition({ x: e.clientX, y: e.clientY });
-			}, 150);
-		};
+  useEffect(() => {
+    const moveCursor = (e) => {
+      // set delay to have magnetic effect
+      setTimeout(() => {
+        let currentIndex = hoveredIndexRef.current;
+        // console.log(currentIndex)
+        if (currentIndex !== null) {
+          certcertListRef.current.textContent = certificates[currentIndex].name;
+          certDescRef.current.textContent = certificates[currentIndex].desc;
+        }
 
-		window.addEventListener("mousemove", moveCursor);
+        setPosition({ x: e.clientX, y: e.clientY });
+      }, 150);
+    };
 
-		return () => {
-			window.removeEventListener("mousemove", moveCursor);
-		};
-	}, []);
+    window.addEventListener("mousemove", moveCursor);
 
-	// Update window width on resize
-	useEffect(() => {
-		const handleResize = () => {
-			setWindowWidth(window.innerWidth);
-		};
-		window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("mousemove", moveCursor);
+    };
+  }, []);
 
-		return () => {
-			window.removeEventListener("resize", handleResize);
-		};
-	}, []);
+  // Update window width on resize
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
 
-	// Pin only if window width > 1024px
-	useLayoutEffect(() => {
-		if (windowWidth > 1024) {
-			let ctx = gsap.context(() => {
-				ScrollTrigger.create({
-					trigger: certDescContainerRef.current,
-					start: "top center",
-					end: "+=550",
-					pin: true,
-					pinSpacing: false,
-				});
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
-				const children = certListRef.current.children;
-				gsap.set(children, { color: "#6a6a6a" });
+  // Pin only if window width > 1024px
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      if (windowWidth > 1024) {
+        ScrollTrigger.create({
+          trigger: certDescContainerRef.current,
+          start: "top center",
+          end: "+=550",
+          pin: true,
+          pinSpacing: false,
+        });
+      }
 
-				[...children].forEach((child, index) => {
-					gsap.to(child, {
-						color: "#ffffff",
-						duration: 0.00001,
-						scrollTrigger: {
-							trigger: child,
-							start: "top center",
-							end: "bottom center",
-							toggleActions: "play reset play reset",
-							markers: true,
-						},
-						// onStart: () => setHoveredIndex(index),
-					});
-				});
-			});
-			return () => ctx.revert(); // cleanup!
-		}
-	}, [windowWidth]); // Dependency on windowWidth
+      const children = certListRef.current.children;
+      gsap.set(children, { color: "#6a6a6a" });
 
-	return (
-		<>
-			<div className="flex flex-col lg:flex-row text-primary  h-full">
-				{hoveredIndex !== null && (
-					<div
-						className="fixed bg-gray-700 w-[400px] h-[350px] transition-transform duration-300 ease-out scale-100"
-						style={{
-							top: `${position.y - 160}px`,
-							left: `${position.x - 180}px`,
-							pointerEvents: "none",
-						}}
-					>
-						<div className="px-10 w-full h-full flex items-center justify-center">
-							<img
-								src={certificates[hoveredIndex].image}
-								alt="certificate"
-								className="shadow-lg shadow-black"
-							/>
-						</div>
-						{/* <div
+      [...children].forEach((child, index) => {
+        gsap.to(child, {
+          color: "#ffffff",
+          duration: 0.00001,
+          scrollTrigger: {
+            trigger: child,
+            start: "top center",
+            end: "bottom center",
+            toggleActions: "play reset play reset",
+            // markers: true,
+          },
+          onStart: () => {
+            setHoveredIndex(index);
+            certcertListRef.current.textContent = certificates[index].name;
+            certDescRef.current.textContent = certificates[index].desc;
+          },
+        });
+      });
+    });
+    return () => ctx.revert(); // cleanup!
+  }, [windowWidth]); // Dependency on windowWidth
+
+  return (
+    <>
+      <div className="flex flex-col lg:flex-row text-primary  h-full">
+        {hoveredIndex !== null && showCertificate && (
+          <div
+            className="fixed bg-gray-700 w-[400px] h-[350px] transition-transform duration-300 ease-out scale-100"
+            style={{
+              top: `${position.y - 160}px`,
+              left: `${position.x - 180}px`,
+              pointerEvents: "none",
+            }}
+          >
+            <div className="px-10 w-full h-full flex items-center justify-center">
+              <img
+                src={certificates[hoveredIndex].image}
+                alt="certificate"
+                className="shadow-lg shadow-black"
+              />
+            </div>
+            {/* <div
 						className="absolute text-white bg-black opacity-70 px-2 py-1"
 						style={{
 							top: "50%", // Vertical center
@@ -120,12 +126,12 @@ const Certificate = () => {
 					>
 						VIEW
 					</div> */}
-					</div>
-				)}
+          </div>
+        )}
 
-				<div className="relative pt-10 px-10 md:pt-15 md:px-15 lg:px-20 lg:pt-15 h-[25vh] lg:h-[300px] lg:w-[60vw] overflow-hidden text-ellipsis  ">
-					<div ref={certDescContainerRef} className=" h-full">
-						{/* <span ref={certcertListRef} className="text-xl text-primary">
+        <div className="relative pt-10 px-10 md:pt-15 md:px-15 lg:px-20  lg:pt-15 h-[25vh] lg:h-[300px] lg:w-[60vw] overflow-hidden text-ellipsis  ">
+          <div ref={certDescContainerRef} className=" h-full">
+            {/* <span ref={certcertListRef} className="text-xl text-primary">
 							Certifications & Achievements:
 						</span>
 						<span className="text-lg md:text-xl text-primary">. </span>
@@ -135,52 +141,58 @@ const Certificate = () => {
 						>
 							Because learning is a muscle—and mine's been lifting
 						</span> */}
-						<span ref={certcertListRef} className="text-xl text-primary">
-							Bangkit Academy led by Google, Tokopedia, Gojek, & Traveloka
-						</span>
-						<span className="text-xl text-primary">. </span>
-						<span
-							ref={certDescRef}
-							className="text-color-text-hovering text-lg"
-						>
-							Bangkit 2023, part of the Kampus Merdeka program, is a
-							collaboration between the Ministry of Education, Culture,
-							Research, and Technology, Google, Tokopedia, Gojek, and Traveloka.
-						</span>
-					</div>
-				</div>
+            <span ref={certcertListRef} className="text-xl text-primary">
+              Bangkit Academy led by Google, Tokopedia, Gojek, & Traveloka
+            </span>
+            <span className="text-xl text-primary">. </span>
+            <span
+              ref={certDescRef}
+              className="text-color-text-hovering text-lg"
+            >
+              Bangkit 2023, part of the Kampus Merdeka program, is a
+              collaboration between the Ministry of Education, Culture,
+              Research, and Technology, Google, Tokopedia, Gojek, and Traveloka.
+            </span>
+          </div>
+        </div>
 
-				<div className="flex flex-col px-5 md:px-10">
-					<span className="text-5xl font-extrabold text-white ">
-						MY CERTIFICATES
-					</span>
-					<div ref={certListRef} className="flex flex-col">
-						{certificates.map((cert, index) => (
-							<a
-								href={cert.link}
-								target="_blank"
-								data-name="certificate"
-								key={index}
-								className="flex gap-5 py-2 md:cursor-none"
-								onMouseEnter={() => setHoveredIndex(index)}
-								onMouseLeave={() => setHoveredIndex(null)}
-							>
-								<span data-name="certificate" className="">
-									{cert.year}
-								</span>
-								<span
-									data-name="certificate"
-									className="text-3xl font-extrabold hover:text-primary"
-								>
-									{cert.name}
-								</span>
-							</a>
-						))}
-					</div>
-				</div>
-			</div>
-		</>
-	);
+        <div className="flex flex-col px-5 md:px-10">
+          <span className="text-5xl font-extrabold text-white ">
+            MY CERTIFICATES
+          </span>
+          <div ref={certListRef} className="flex flex-col">
+            {certificates.map((cert, index) => (
+              <a
+                href={cert.link}
+                target="_blank"
+                data-name="certificate"
+                key={index}
+                className="flex gap-5 py-2 md:cursor-none"
+                onMouseEnter={() => {
+                  setHoveredIndex(index);
+                  setShowCertificate(true);
+                }}
+                onMouseLeave={() => {
+                  setHoveredIndex(null);
+                  setShowCertificate(false);
+                }}
+              >
+                <span data-name="certificate" className="">
+                  {cert.year}
+                </span>
+                <span
+                  data-name="certificate"
+                  className="text-3xl font-extrabold hover:text-primary"
+                >
+                  {cert.name}
+                </span>
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Certificate;
